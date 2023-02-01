1973c636804c12161c4396dba1940ef9ae40e39c
Jose Antonio Carmona <jcarmona@vmware.com>
2023-02-01 11:43:04 +0100

    Fix check-components
    Signed-off-by: Jose Antonio Carmona <jcarmona@vmware.com>

--- .vib/common/goss/scripts/check-components.sh
+++ .vib/common/goss/scripts/check-components.sh
@@ -5,6 +5,8 @@ components=( $(cat /opt/bitnami/.bitnami_components.json | grep ": {" | cut -d '
 for component in "${components[@]}"; do
   if [[ "$component" == "mariadb" ]]; then
     which mysql
+  elif [[ "$component" == "apache" ]]; then
+    which apachectl
   else
     which $component
   fi
